0c8ee3f122731dc69bbea634261722b648ca10c8
Francisco Remiro <fremigom@myuax.com>
2026-04-17 14:06:10 +0200

    actualizado

--- listas.py
+++ listas.py
@@ -2,6 +2,8 @@ colores=[] #Lista vacía
 colores=['Rojo', 'Verde', 'Azul']
 print("colores:", colores)
 print("colores[0]:",colores[0])
+
+
 colores[0]='Blanco'
 print(f"colores: {colores}, colores[0]: {colores[0]}")
 colores.append('Amarillo')
@@ -10,9 +12,23 @@ print("colores:", colores)
 for i in colores:
     print(i)
 
+
+print("colores:", colores)
+#col=input("Dime un color:")
+col='Azul'
+try:
+    pos= colores.index(col)
+    print(f"Color indicado: {col}, posicion en lista: {pos}")
+except Exception as e:
+    print("El color indicado no se encuentra en la lista")
+
+print("La ejecución del programa continua")
+
+
+
+
 coloresMin=[ i.lower() for i in colores ]
 print(coloresMin)
-
 #color= input("Dime un color...")
 color="VERde"
 try: 
@@ -27,6 +43,7 @@ print("colores:", colores)
 
 try:
     colores.remove("Grises")
+
 except:
     print(f"El color a borrar no está en la lista")
 
@@ -63,11 +80,42 @@ print("listaNum3:", listaNum3)
 listaNum3.extend(listaNum3)
 print("listaNum3:", listaNum3)
 
+#El elemento en la posición 6 no está incluido. Va de 1 a 5
 listaNum4= listaNum3[1:6]
 print("listaNum4:", listaNum4)
 
+#Uso de rangos con salto
 listaNum5Pares= listaNum3[1:len(listaNum3):2]
 print("listaNum5Pares:", listaNum5Pares)
 
-# [indice de comienzo::]
-print("Rango listaNum3:",listaNum3[:2:])
+#En los rangos podemos omitir valores. Si omitimos el inicio
+#el rango comenzará en el primero de la lista. Si omitimos el
+#fin, el rango irá hasta el último elemento de la lista. Si omitimos
+#el salto, se toma con salto el valor 1
+print("Rango listaNum3", listaNum3[6::])
+
+#Se pueden utilizar rangos negativos para recorrer la lista
+print("Rango listaNum3", listaNum3[-1:-13:-1])
+
+print("Rango listaNum3", listaNum3[-1::-1])
+
+listaNum6= ['A', 'B', 'C']
+listaNum7= [1, 2, 3]
+
+#Podemos definir sublistas
+listaNum8 = [listaNum6, listaNum7]
+print("listaNum8:", listaNum8)
+
+#Acceso a un elemento de una sublista
+print("listaNum8[1][1]:", listaNum8[1][1])
+listaNum9= ['X', 'Y', 'Z']
+
+listaNum8.append(listaNum9)
+print("listaNum8:", listaNum8)
+
+listaNum8.extend(listaNum9)
+print("listaNum8:", listaNum8)
+
+#Las listas pueden tener elementos de distinto tipo
+listaNum10=[125, 'A', 2.3, True, [1, 2, 3]]
+print("listaNum10:", listaNum10)
